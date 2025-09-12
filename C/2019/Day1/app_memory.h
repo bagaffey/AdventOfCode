65c3caac5b7646cb1367e9c4a50a710e4a0f15bc
Barry Gaffey <bagaffey@users.noreply.github.com>
2025-09-11 20:33:54 -0400

    Added helper constructors and arena params.

--- C/2019/Day1/app_memory.h
+++ C/2019/Day1/app_memory.h
@@ -46,7 +46,7 @@ typedef struct arena_push_params
 {
 	u32 Flags;
 	u32 Alignment;
-};
+} arena_push_params;
 
 inline arena_push_params
 DefaultArenaParams(void)
@@ -54,5 +54,67 @@ DefaultArenaParams(void)
 	arena_push_params Params;
 	Params.Flags = ArenaFlag_ClearToZero;
 	Params.Alignment = 4;
-	return(Parms);
+	return(Params);
 }
+
+inline arena_push_params
+AlignNoClear(u32 Alignment)
+{
+	arena_push_params Params = DefaultArenaParams();
+	Params.Flags &= ~ArenaFlag_ClearToZero;
+	Params.Alignment = Alignment;
+	return(Params);
+}
+
+inline arena_push_params
+Align(u32 Alignment, bool32 Clear)
+{
+	arena_push_params Params = DefaultArenaParams();
+	if (Clear)
+		Params.Flags |= ArenaFlag_ClearToZero;
+	else
+		Params.Flags &= ~ArenaFlag_ClearToZero;
+	Params.Alignment = Alignment;
+	return(Params);
+}
+
+inline arena_push_params
+NoClear(void)
+{
+	arena_push_params Params = DefaultArenaParams();
+	Params.Flags &= ~ArenaFlag_ClearToZero;
+	return(Params);
+}
+
+typedef struct arena_bootstrap_params
+{
+	u64 AllocationFlags;
+	umw MinimumBlockSize;
+} arena_bootstrap_params;
+
+inline arena_bootstrap_params
+DefaultBootstrapParams(void)
+{
+	arena_bootstrap_params Params = {};
+	return(Params);
+}
+
+inline arena_bootstrap_params
+NonRestoredArena(void)
+{
+	arena_bootstrap_params Params = DefaultBootstrapParams();
+	Params.AllocationFlags = PlatformMemory_NotRestored;
+	return(Params);
+}
+
+#if PROJ_INTERNAL
+#define DEBUG_MEMORY_LANE(Name) DEBUG_NAME_(__FILE__, __LINE__, __COUNTER__),
+#define INTERNAL_MEMORY_PARAM char *GUID,
+#define INTERNAL_MEMORY_PASS GUID,
+#else
+#define DEBUG_MEMORY_NAME(Name)
+#define INTERNAL_MEMORY_PARAM
+#define INTERNAL_MEMORY_PASS
+#endif
+
+#define
