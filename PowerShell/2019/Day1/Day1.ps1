3415f0730802586c4dddd797ff494558fcf2429e
Barry Gaffey <bagaffey@users.noreply.github.com>
2025-09-25 21:34:39 -0400

    Adding first ps1 script

---
+++ PowerShell/2019/Day1/Day1.ps1
@@ -0,0 +1,9 @@
+<#
+.SYNOPSIS
+    Advent of Code 2019 Day 1 Fuel Solution in PowerShell
+
+.DESCRIPTION
+    - If an input path is provided, reads masses from that file.
+    - Otherwise, uses a cached file in %TEMP% (2019-12-01-1.dat).
+    If not cached, downloads from AoC using AOC_SESSION cookie and caches it.
+#>
